493af6286267fd922b0c6e4ce9300d5f31ccd56f
Pereyra Hugo <62813900+pereyrahugor@users.noreply.github.com>
2026-01-12 14:12:27 -0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-    input: 'src/app.ts',
+    input: ['src/app.ts', 'scripts/init_functions.ts'],
     output: {
         dir: 'dist',
         format: 'esm',
+        entryFileNames: chunk => {
+            if (chunk.facadeModuleId.includes('scripts')) {
+                return 'scripts/[name].js';
+            }
+            return '[name].js';
+        }
     },
     onwarn: (warning) => {
         if (warning.code === 'UNRESOLVED_IMPORT') return
