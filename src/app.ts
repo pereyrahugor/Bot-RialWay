39480381ed9e3aac386bdfac8c5c98cbbf2988f4
pereyrahugor <pereyrahugor@gmail.com>
2026-06-22 01:21:42 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -147,7 +147,7 @@ const main = async () => {
     const qrPath = path.join(process.cwd(), "bot.qr.png");
 
     // Usar versión fija conocida que funciona
-    const baileysVersion: any = [2, 3000, 1040753655]; //Vencimineto = 
+    const baileysVersion: any = [2, 3000, 1040769081]; //Vencimineto = 
     console.log(`📡 [App] Usando versión fija de WhatsApp Web: ${baileysVersion.join('.')}`);
 
     // 2. Initialize Providers
